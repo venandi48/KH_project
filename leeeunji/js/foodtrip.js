7413ca3fed6d54f55f33f8ee39fdfbd1b705a219
venandi48 <bow999@naver.com>
2022-05-01 18:04:40 +0900

    220501 foodtrip - add data, writing js, bgm onoff

--- leeeunji/js/foodtrip.js
+++ leeeunji/js/foodtrip.js
@@ -18,17 +18,123 @@ const reviewPostingAll = [];
 const addTempData = () => {
     let postingNo = 1;
 
-    reviewPostingAll.push(new Review(postingNo++, "20220420_001", new Date("2021/12/25"), "인천광역시 미추홀구 관교동", "테리토리", 4.5, "./images/foodtrip/foodtrip_20220420_001.jpg",
-    `음냐음냐 맛나당`));
-    reviewPostingAll.push(new Review(postingNo++, "20220420_002", new Date("2022/02/26"), "인천광역시 중구", "을왕리짱구네", 4.3, "./images/foodtrip/foodtrip_20220420_002.jpg",
+    reviewPostingAll.push(new Review(postingNo++, "20220420_001", new Date("2021/12/25"), "인천광역시 미추홀구 관교동", "테리토리", 4.3, "./images/foodtrip/foodtrip_20220420_001.jpg",
+`메뉴 플레이팅을 예쁘게 해준다. 보통 플레이팅 예쁜 인스타감성 브런치집은 맛없는 경우가 종종 있는데, 여기는 맛도 괜찮고 좋다. 시즌별로 메뉴가 바뀌니 시즌메뉴가 마음에 든다면 없어지기전에 먹어봐야한다.
+주차공간은 따로 없다. 근처 골목에 적당한 주차공간 찾기가 어려울 수 있으니 걸어가는게 좋다.
+그리고 보통 식당이나 카페는 후불제인데, 이곳은 선불제로 운영한다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20220420_002", new Date("2022/02/26"), "인천광역시 중구", "을왕리짱구네", 4.5, "./images/foodtrip/foodtrip_20220420_002.jpg",
 `매콤한 연포탕이라고 생각하면된다. 메뉴는 '빨간거', '하얀거' 이렇게 파는데 빨간게 맛있다.
-직원분들이 알아서 조리해주고 낙지도 잘라주셔서 먹어도 된다고 할 때 까지 국자들 필요도 없어 편하다.
+직원분들이 알아서 조리해주고 낙지도 잘라주셔서 먹어도 된다고 할 때 까지 국자에 손 댈 필요도 없어 편하다.
 그리고 사리로 고기를 추가하면 두배로 맛있다. 지금까지 세 번 방문했는데 또 가고싶다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20220501_001", new Date("2022/04/30"), "인천광역시 중구", "꾸덕집", 4.7, "./images/foodtrip/foodtrip_20220501_001.jpg",
+`생선구이가 정말 맛있다. 모둠으로 시키면 가자미, 고등어, 임연수 구이와 장어튀김이 나온다. 밥은 밥솥째 나와서 처음엔 당황스러울 수 있다. 같이 나오는 된장찌개에는 일반두부 대신에 순두부가 숭덩숭덩 들어가있다.
+제주금게장도 살 많은 게장이라 진짜 밥도둑이다. 웨이팅 있을 수 있으니 기다리기 싫다면 조금 일찍가거나 늦게 가야한다. 화장실도 의외로 깔끔하다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20220501_002", new Date("2022/02/06"), "경기도 시흥시", "릴리프커피", 3.8, "./images/foodtrip/foodtrip_20220501_002.jpg",
+`인테리어가 우드톤이기도하고, 돌을 활용해서 따뜻하고 고즈넉한 느낌을 준다. 주차공간이 마련되어있어서 차끌고 가기에도 괜찮다. 소품류와 원두도 판매하고있어서 커피나오는동안 잠시 둘러보기 좋다.
+주말에는 사람이 많아서 생각만큼 조용한 분위기가 나지 않을 수 있다.
+흑임자케이크가 괜찮았다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20210501_003", new Date("2021/12/23"), "서울특별시 강남구 청담동", "옳음", 4.5, "./images/foodtrip/foodtrip_20220501_003.jpg",
+`청담동에 있는 모던 한식 파인다이닝이고, 미쉐린 가이드에도 소개되었다.
+방문일에는 룸 예약자가 없어서, 가장 먼저 예약했다고 룸으로 안내해주셨다.
+개인적으로 트러플파스타는... 트러플 좋아하는 사람들은 좋았겠지만, 나는 트러플 싫어해서 별로였다.
+그래도 코스가 전반적으로 맛있는데, 특히 금태요리가 충격적으로 부드럽고 맛있다.
+모던 한식 파인다이닝이라 한식 재료와 서양식 재료를 같이 사용한 코스가 많은데, 굉장히 신선하다.
+가격은.. 비싸긴해도 뭐 맛있으니까 특별한 날에 한번쯤 갈만하다고 생각한다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20210501_004", new Date("2021/10/17"), "서울특별시 영등포구 문래동", "머트리얼", 3.7, "./images/foodtrip/foodtrip_20220501_004.jpg",
+`지하에 있어서 채광은 없고, 왠지 환기가 안되지않을까 하는 걱정이 들었다. 그래도 화장실 깨끗한걸 보면 환기도 알아서 잘 하지않을까?
+오후에 갔더니 바스크치즈케이크는 품절이라 못먹었다. 다른 디저트는 특별하게 맛있는건 아니여도 괜찮았다.`));
+    reviewPostingAll.push(new Review(postingNo++, "20210501_005", new Date("2021/05/21"), "서울특별시 마포구 도화동", "뫼촌", 4.0, "./images/foodtrip/foodtrip_20220501_005.jpg",
+`최자로드맛집이라는데... 최자로드는 잘 모르지만, 맛있긴하다.
+웨이팅도 있다. 저녁타임에는 술도 같이 시켜먹는 테이블이 많다보니 웨이팅이 잘 안빠진다.
+일반 닭볶음탕이랑 달리 국물이 없어서 밥 비벼서 먹진 못하니까 살짝 아쉽다.
+그리고 은은하게 불닭볶음면 정도의 맵기가 올라와서 맵찔이는 땀이 날 수 있다.
+감자전은 같이 시켜먹으면 덜 매운 것 같기도하고, 일단 감자전은 맛있으니까 시켜먹는게 좋다.`));
+};
+
+// 신규 포스팅 버튼에 클릭 이벤트 추가
+const addWritingListener = () => {
+    const target = document.querySelector("#creatPostingBtn");
+
+    target.addEventListener("click", () => {
+        const backgroundPlate = document.getElementById("backgroundPlate");
+        const writingPlate = document.getElementById("writingPlate");
+        
+        backgroundPlate.classList.add("reviewSection");
+        backgroundPlate.classList.remove("visibilityHidden");
+
+        writingPlate.classList.add("writing-wrapper-back");
+        writingPlate.classList.remove("visibilityHidden");
+    });
+};
+
+// 제출시
+const postingValSubmit = () => {
+    let result = true;
+
+    document.postingFrm.onsubmit = (e) => {
+
+        // 위치
+        // 입력가능문자 검사 -> 행정구역 검사 
+        const locationVal = addRst_location.value;
+        if(!/^[가-힣]+\s?[가-힣0-9\-]*$/g.test(locationVal)){
+            printFailedMsg("writingLocationRow", "한글, 띄어쓰기 한 개만 사용하세요.");
+            result = false;
+        } else {
+            const dividedLocation = locationVal.split(" ");
+            if(!/^.+[시군구]$/.test(dividedLocation[0])){
+                printFailedMsg("writingLocationRow", "시/군/구가 없습니다.");
+                result = false;
+            }
+            if(dividedLocation[1] && !/^.+([동읍리]|[길로]([0-9]*|[0-9]+-[0-9]+))$/.test(dividedLocation[1])) {
+                printFailedMsg("writingLocationRow", "적절한 동/읍/리/길/로가 아닙니다.");
+                result = false;
+            } else resetMsg("writingLocationRow");
+        }
+        
+        // 평점
+        if(!/^([0-5]|[0-4].[0-9]|5.0)$/.test(addRst_score.value)){
+            printFailedMsg("writingScoreRow", "0~5 사이의 소수점 한자리 이내로 입력하세요.");
+            result = false;
+        } else resetMsg("writingScoreRow");
+
+        return result;
+    };
+
+}
+
+const printFailedMsg = (targetClass, msg) => {
+    document.querySelector(`.${targetClass} span.testFailedMsg`).innerHTML = msg;
+};
+const resetMsg = (targetClass) => {
+    document.querySelector(`.${targetClass} span.testFailedMsg`).innerHTML = "";
+};
+
+const addCloseWritingListener = () => {
+    const target = document.querySelector("#closeWritingBtn");
+
+    target.addEventListener("click", () => {
+        // 입력내용 초기화
+        const inputAll = document.querySelectorAll("#writingTb input");
+        inputAll.forEach((input, index) => {
+            input.value = "";
+        });
+        document.querySelector("#writingTb select").value = "서울특별시";
+        document.querySelector("#writingTb textarea").value = "";
+
+        const backgroundPlate = document.getElementById("backgroundPlate");
+        const writingPlate = document.getElementById("writingPlate");
+        
+        // hidden으로 변경
+        backgroundPlate.classList.add("visibilityHidden");
+        backgroundPlate.classList.remove("reviewSection");
+
+        writingPlate.classList.add("visibilityHidden");
+        writingPlate.classList.remove("writing-wrapper-back");
+    });
 };
 
 const addOpenReviewListener = () => {
     const reviews = document.querySelectorAll("#postingBoardDiv li");
-    // console.log(reviews);
 
     // 각 리뷰 클릭시 리뷰창 열리는 이벤트리스너 추가
     reviews.forEach((review, index) => {
@@ -120,6 +226,21 @@ function twoDigitNum(num) {
     return num < 10 ? "0"+num : num;
 }
 
+const unmuteMusic = () => {
+    const target = document.querySelector("#musicStartBtn")
+    target.addEventListener("click", () => {
+        const bgm = document.querySelector("#bgm");
+        const bgmSrc = bgm.src;
+        if(bgmSrc.indexOf("&mute=false") >= 0){
+            bgm.src = bgmSrc.replace("&mute=false", "&mute=true");
+            target.innerText = "start music";
+        } else {
+            bgm.src = bgmSrc.replace("&mute=true", "&mute=false");
+            target.innerText = "stop music";
+        }
+    });
+}
+
 const init = () => {
     // 임의 데이터 추가
     addTempData();
@@ -133,4 +254,12 @@ const init = () => {
     addOpenReviewListener();
     addcloseReviewListener();
 
+    // 신규포스팅 작성 버튼, 닫기버튼 이벤트리스너 추가
+    addWritingListener();
+    addCloseWritingListener();
+
+    // bgm 재생버튼
+    unmuteMusic();
+
+    postingValSubmit();
 };
